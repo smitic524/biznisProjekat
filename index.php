09feb3e983fcc9d84163d5466c7f5fa938ff980c
Sasa Mitic <sasamitic96@gmail.com>
2020-07-29 22:59:10 +0200

    Zavrsen biznis projekat

---
+++ index.php
@@ -0,0 +1,146 @@
+<?php include('partials/header.php'); ?>
+
+
+<!--- header --->
+
+<header>
+    <h1>Trusted and <br> proffesionals advisers</h1>
+    <h3>For your business</h3>
+    <p>Showcase your Profile to the world using online CV builder and Get Hired Faster</p>
+    <button>About us</button>
+</header>
+
+<!--- headings --->
+    <section class="headings container">
+        <article>
+            <h2>01</h2>
+        </article>
+        <article>
+            <h3>About us</h3>
+            <h2><span>WE CAN</span> HELP YOUR BUSINESS GROW</h2>
+            <h4>Get your Business in 4 easy steps</h4>
+        </article>
+    </section>
+
+
+<!--- about --->
+
+<section class="about">
+        <article>
+            
+        </article>
+        <article>
+            <h2>EVERYTHING YOU NEED IN ONE SOLUTION</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia sed voluptatibus ipsa dolorum voluptate. Voluptate animi vero corporis dicta ipsum reiciendis quae porro nobis nam nisi, maxime provident voluptatem explicabo laudantium quod voluptas assumenda laboriosam tempore asperiores error? Nobis maiores assumenda accusamus qui illum quaerat quas nihil, optio voluptates neque pariatur veniam error voluptatibus molestias nam exercitationem labore minima id.</p>
+        </article>
+</section>
+<!-- headings -->
+<section class="headings container">
+        <article>
+            <h2>02</h2>
+        </article>
+        <article>
+            <h3>Services</h3>
+            <h2><span>HOW TO PROVIDE</span> TOOLS THAT HELP ANYONE</h2>
+            <h4>Get your Business in 4 easy steps</h4>
+        </article>
+    </section>
+<!-- services -->
+    <section class="container services">
+        <article>
+            <i class="far fa-chart-bar"></i>
+            <h3>Marketing</h3>
+            <hr>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nihil necessitatibus quas quaerat laborum architecto debitis. Similique alias ab voluptates!</p>
+        </article>
+        <article>
+            <i class="fas fa-bullseye"></i>
+            <h3>Marketing</h3>
+            <hr>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nihil necessitatibus quas quaerat laborum architecto debitis. Similique alias ab voluptates!</p>
+        </article><article>
+            <i class="fas fa-map-signs"></i>
+            <h3>Marketing</h3>
+            <hr>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nihil necessitatibus quas quaerat laborum architecto debitis. Similique alias ab voluptates!</p>
+        </article><article>
+            <i class="fas fa-filter"></i>
+            <h3>Marketing</h3>
+            <hr>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nihil necessitatibus quas quaerat laborum architecto debitis. Similique alias ab voluptates!</p>
+        </article><article>
+            <i class="fas fa-hand-holding-usd"></i>
+            <h3>Marketing</h3>
+            <hr>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nihil necessitatibus quas quaerat laborum architecto debitis. Similique alias ab voluptates!</p>
+        </article><article>
+            <i class="fas fa-map-signs"></i>
+            <h3>Marketing</h3>
+            <hr>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nihil necessitatibus quas quaerat laborum architecto debitis. Similique alias ab voluptates!</p>
+        </article>
+    </section>
+<!-- personal business -->
+
+    <section class="personalBusiness">
+            <div class="headings container">
+                <article>
+                    <h2>03</h2>
+                </article>
+                <article>
+                    <h3>Services</h3>
+                    <h2><span>HOW TO PROVIDE</span> TOOLS THAT HELP ANYONE</h2>
+                    <h4>Get your Business in 4 easy steps</h4>
+                    <button>About us</button>
+                </article>
+            </div>
+            <div class="container counter">
+                <article>
+                    <h2>18</h2>
+                    <h4>Nomination</h4>
+                    <hr>
+                </article>
+                <article>
+                    <h2>5</h2>
+                    <h4>Awards</h4>
+                    <hr>
+                </article><article>
+                    <h2>48</h2>
+                    <h4>Agencies</h4>
+                    <hr>
+                </article>
+            </div>
+
+    </section>
+
+    <section class="headings container">
+        <article>
+            <h2>04</h2>
+        </article>
+        <article>
+            <h3>Our team</h3>
+            <h2><span>WE HAVE A PROFFESIONAL</span> TEAM OF BUSINESS ANALYSTS</h2>
+            <h4>Get your Business in 4 easy steps</h4>
+        </article>
+    </section>
+    <!-- team -->
+
+    <section class="container team">
+        <article>
+            <img src="img/team1.jpg" alt="">
+            <h4>James Douglas</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam fuga vero at. Quas, officia. Voluptatum eveniet optio ad dignissimos praesentium doloremque, aperiam suscipit soluta dolorum commodi facere labore expedita blanditiis!</p>
+        </article>
+        <article>
+            <img src="img/team2.jpg" alt="">
+            <h4>Peter Green</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam fuga vero at. Quas, officia. Voluptatum eveniet optio ad dignissimos praesentium doloremque, aperiam suscipit soluta dolorum commodi facere labore expedita blanditiis!</p>
+        </article><article>
+            <img src="img/team3.jpg" alt="">
+            <h4>Ella Williams</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam fuga vero at. Quas, officia. Voluptatum eveniet optio ad dignissimos praesentium doloremque, aperiam suscipit soluta dolorum commodi facere labore expedita blanditiis!</p>
+        </article>
+    </section>
+
+
+<?php include('partials/footer.php'); ?>
---
+++ partials/footer.php
@@ -0,0 +1,25 @@
+ <!--- FOOTER --->
+ <footer>
+        <div class="container">
+            <div class="col">
+            <a href="" class="logo">BUSINESS <br> <span>Marketing &amp; consulting</span></a>
+            </div>
+            <div class="col">
+                <i class="fas fa-map-marker-alt"></i>
+                    <p>Marsala Tita 3, Apatin</p>
+                    <p>Srbija</p>
+            </div>
+            <div class="col">
+                <i class="fas fa-envelope"></i>
+                    <p>info@biznis.com</p>
+                    <p>office@biznis.com</p>
+            </div>
+            <div class="col">
+                <i class="fas fa-phone"></i>
+                    <p>062 222 333</p>
+                    <p>063 333 555</p>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
